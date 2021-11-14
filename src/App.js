91add238a7b3a8e2efa5270e8234f720b21b901c
Salil Naik <salil.naik27@gmail.com>
2021-11-14 07:26:45 +0530

    add: wallet/metamask connector

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
+import { Web3ReactProvider } from "@web3-react/core";
+import Web3 from "web3";
 import "./styles/app.scss";
 import { Header } from "./components/index";
 import { Homepage } from "./pages/Homepage/Homepage";
 
+function getLibrary(provider) {
+  return new Web3(provider);
+}
+
 function App() {
-  return <div className="App">
-    <Header />
-    <Homepage />
-  </div>;
+  return (
+    <div className="App">
+      <Web3ReactProvider getLibrary={getLibrary}>
+        <Header />
+        <Homepage />
+      </Web3ReactProvider>
+    </div>
+  );
 }
 
 export default App;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,31 @@
 import style from "./header.module.scss";
 import logo from "../../assets/images/logo.png";
+import { useWeb3React } from "@web3-react/core";
+import { injected } from "../Wallet/Connector";
 
 export const Header = () => {
-    return (
-        <header className={style.header}>
-            <div className="d-flex align-items-center">
-            <img src={logo} alt="website logo" className={style.logo}/>
-            <span className={style.name}>Movr Network</span>
-            </div>
-        </header>
-    )
-}
+  const { active, account, activate, deactivate } =
+    useWeb3React();
+
+  async function connect() {
+    try {
+      await activate(injected);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  return (
+    <header className={style.header}>
+      <div className="d-flex align-items-center justify-content-between">
+        <div className="d-flex align-items-center">
+          <img src={logo} alt="website logo" className={style.logo} />
+          <span className={style.name}>Movr Network</span>
+        </div>
+        <button onClick={connect}>
+          {active ? account : "Connect your wallet"}
+        </button>
+      </div>
+    </header>
+  );
+};
